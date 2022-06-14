062be75024f7eb2869e140e8f9d3389471a94104
bosukeme <ukeme.wilson@gmail.com>
2022-06-14 13:09:19 +0100

    files

--- app.py
+++ app.py
@@ -2,40 +2,41 @@ from flask import Flask
 from views import user_bp
 
 
-# def create_app(settings_override=None):
-#     """
-#     Create a Flask application using the app factory pattern.
+def create_app(settings_override=None):
+    """
+    Create a Flask application using the app factory pattern.
 
-#     :param settings_override: Override settings
-#     :return: Flask app
-#     """
+    :param settings_override: Override settings
+    :return: Flask app
+    """
 
-#     app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__, instance_relative_config=True)
 
-#     app.config.from_object('config.settings')
-#     app.config.from_pyfile('settings.py', silent=True)
+    app.config.from_object('config.settings')
+    app.config.from_pyfile('settings.py', silent=True)
 
-#     if settings_override:
-#         app.config.update(settings_override)
+    if settings_override:
+        app.config.update(settings_override)
 
-#     app.logger.setLevel('DEBUG')
-#     app.register_blueprint(user_bp)
+    app.logger.setLevel('DEBUG')
+    app.register_blueprint(user_bp)
     
-#     app.secret_key = "lpage"
+    app.secret_key = "lpage"
 
-#     return app
-
-
+    return app
 
 
-app = Flask(__name__)
 
-app.secret_key = "SECRET_KEY"
 
-app.register_blueprint(user_bp)
+# app = Flask(__name__)
+
+# app.secret_key = "SECRET_KEY"
+
+# app.register_blueprint(user_bp)
 
 
 if __name__=='__main__':
+    app = create_app()
     app.run(port= 5000, debug=True)
 
 
