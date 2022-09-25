57dc174c0982245e88212cd6c2fd3345960f6525
bloverseukeme <ukeme.wilson@bloverse.com>
2022-09-25 22:33:42 +0100

    new updates

--- views.py
+++ views.py
@@ -1,17 +1,37 @@
 from flask import Blueprint, render_template, Response
 from flask_restful import Api, Resource
+import random
 
 # from resources.user_resource import Registration, Login
 # from resources.user_resource import Templates, TemplatesUpdate
 
+SKILLS = [ 
+    "SQL", "FLASK", "SPACY",
+    "DOCKER", "PANDAS", "NUMPY",
+    "PYMONGO","TWEEPY","TWINT",
+    "SELENIUM","REQUESTS","OPENCV",
+    "DOCKER SWARM", "SQLALCHEMY",
+    "SKLEARN", "REGEX", "REDIS", 
+    "PILLOW", "NLTK", "MATPLOTLIB",
+    "HCLOUD", "DUCKDUCKGO", "DJANGO",
+    "CELERY", "CLOUDINARY"
+]
+
+
+PERCENT_FILL = [random.randint(79,95) for item in range(len(SKILLS))]
+
+SKILLS_PERCENT = tuple(zip(SKILLS, PERCENT_FILL))
+
+
 class Home(Resource):
     def get(self):
-        return Response(response=render_template('blog.html'))
+        # return Response(response=render_template('blog.html'))
+        return Response(response=render_template('Home.html'))
 
 
 class Skills(Resource):
     def get(self):
-        return Response(response=render_template('skills.html'))
+        return Response(response=render_template('skills.html', SKILLS_PERCENT = SKILLS_PERCENT))
 
 
 class Medium(Resource):
@@ -33,3 +53,6 @@ api.add_resource(Medium, '/medium')
 # api.add_resource(Login, "/login")
 # api.add_resource(Templates, "/template")
 # api.add_resource(TemplatesUpdate, "/template/<string:template_id>")
+
+
+# clear-mistletoe-zmutw53pjmb5gyuc5y3agu2u.herokudns.com
